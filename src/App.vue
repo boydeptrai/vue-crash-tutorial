0c85f818c2e967760c3b53ffa261ef17dfd66c2d
Tuan Tran <langthamht047@gmail.com>
2022-11-20 19:53:28 -0800

    initial code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <TheHeader />
+    <TodoItem />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader';
+import TodoItem from './components/TodoItem.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TheHeader,
+    TodoItem
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Arial, Helvetica, sans-serif;
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>Viec 1</p>
+  <p>Viec 1</p>
+  <p>Viec 1</p>
+</template>
+
+<script>
+export default {
+   name: 'TodoItem'
+}
+</script>
+
+<style>
+
+</style>
