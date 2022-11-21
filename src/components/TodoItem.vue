bf7b4a8d5039eee667fa16101624b0dbcf658286
Tuan Tran <langthamht047@gmail.com>
2022-11-21 03:00:22 -0800

    emit event

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,35 +1,51 @@
 <template>
-  <TodoListItem v-for="todo in todos" :key="todo.id" :todoProps="todo" />
+  <TodoListItem
+    v-for="todo in todos"
+    :key="todo.id"
+    :todoProps="todo"
+    @item-completed="markComplete"
+    @delete-item="deleteTodo"
+  />
 </template>
 
 <script>
 import { ref } from 'vue';
-import TodoListItem from './TodoListItem.vue'
+import TodoListItem from './TodoListItem.vue';
 export default {
   name: 'TodoItem',
   components: {
-    TodoListItem
+    TodoListItem,
   },
   setup() {
     const todos = ref([
-        {
-            id: "1",
-            title: 'Viec 1',
-            completed: true
-        },
-        {
-            id: "2",
-            title: 'Viec 2',
-            completed: false
-        },
-        {
-            id: "3",
-            title: 'Viec 3',
-            completed: false
-        },
+      {
+        id: '1',
+        title: 'Viec 1',
+        completed: false,
+      },
+      {
+        id: '2',
+        title: 'Viec 2',
+        completed: false,
+      },
+      {
+        id: '3',
+        title: 'Viec 3',
+        completed: false,
+      },
     ]);
+    
+    const markComplete = id =>{
+     todos.value =todos.value.map((todo) =>{
+      if (todo.id === id) todo.completed = !todo.completed
+      return todo
+     })
+    }
 
-    return { todos };
+    const deleteTodo = id =>{
+      todos.value = todos.value.filter(todo =>todo.id !== id)
+    }
+    return { todos,markComplete,deleteTodo };
   },
 };
 </script>
